70ffa571aa890486e89e0d39fa20c1cbda738584
Aluno <Aluno@fatecdiadema.com>
2023-01-26 08:27:54 -0300

    Diretório rotas e direcionamento index

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 //Fazendo a importação do express
 const express = require('express');
-
-//Notas (Quando o servidor for acessado, para onde ele vai?)
-const router = express.Router();
-router.get('/',(req, res) => {
-    res.send("Eu consegui...");    
-});
+const router = require('./routes/index');
 
 //Configurações básicas do aplicativo
 const app = express();
